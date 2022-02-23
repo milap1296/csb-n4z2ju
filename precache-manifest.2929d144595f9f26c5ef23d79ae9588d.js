c0ea1ad40d2ea807e0cb128772413cc9f03f5273
Code Sandbox <hello@codesandbox.io>
2022-02-23 20:34:42 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.2929d144595f9f26c5ef23d79ae9588d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39a9a3b5e12b3fa153a5dd31aff2aa84",
+    "url": "https://.github.io/csb-n4z2ju/index.html"
+  },
+  {
+    "revision": "60c4983275708fefa753",
+    "url": "https://.github.io/csb-n4z2ju/static/css/main.7190c8e8.chunk.css"
+  },
+  {
+    "revision": "e67ed16403b8da2ba367",
+    "url": "https://.github.io/csb-n4z2ju/static/js/2.e53e0ff9.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "https://.github.io/csb-n4z2ju/static/js/2.e53e0ff9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60c4983275708fefa753",
+    "url": "https://.github.io/csb-n4z2ju/static/js/main.03f88174.chunk.js"
+  },
+  {
+    "revision": "4e5c6aaaa01b7d51b35f",
+    "url": "https://.github.io/csb-n4z2ju/static/js/runtime-main.3b3e5f2a.js"
+  },
+  {
+    "revision": "fa674d40740705aaaa432bd07bf556df",
+    "url": "https://.github.io/csb-n4z2ju/static/media/milapshah.fa674d40.png"
+  },
+  {
+    "revision": "1d6135627059d17082bf5be5fef47364",
+    "url": "https://.github.io/csb-n4z2ju/static/media/resume.1d613562.pdf"
+  }
+]);
